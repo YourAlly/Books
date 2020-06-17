dcb13ae47895bdf514f001e21274a0ece49026ec
YourAlly <Royettemiranda@YAHOO.COM>
2020-06-18 01:03:17 +0800

    fixed import.py

--- import.py
+++ import.py
@@ -9,17 +9,20 @@ if not os.getenv("DATABASE_URL"):
 
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
+def main():
+    with open("books.csv") as file:
+        books = csv.reader(file)
+        start = True
+        for isbn, title, author, year in books:
+            print(isbn)
+            if not start:
+                db.execute("INSERT INTO books(isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+                {"isbn":isbn, "title":title, "author":author, "year": int(year) })
+            else:
+                start = False
 
-with open("books.csv") as file:
-    books = csv.reader(file)
-    start = True
-    for isbn, title, author, year in books:
-        print(isbn)
-        if not start:
-            db.execute("INSERT INTO books(isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
-            {"isbn":isbn, "title":title, "author":author, "year": int(year) })
-        else:
-            start = False
+        db.commit()
+        print("DONE!")
 
-    db.commit()
-    print("DONE!")
+if __name__ == '__main__':
+    main()
